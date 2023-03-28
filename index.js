bbebc8d7464b2df0f8692885df42dd9cf1b18450
Israel Brassell <israel.brassell@gmail.com>
2023-03-28 17:13:42 -0400

    :)

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { Manager, Engineer, Intern } = require('./lib');
+const Manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern')
 const inquirer = require('inquirer');
 const { resolve, join } = require('path');
 const fs = require('fs');
